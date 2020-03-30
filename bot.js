67778ba149b8dc332535cbf8fe5f88ea3b0f7d77
Austin Gallippo <phantomwolf154@gmail.com>
2020-03-30 15:43:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,18 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
+var fuckyou = "~"
 
 bot.on("ready", () => {
   bot.user.setStatus("\"Hello there\" - General Kenobi.");
+});
+
+bot.on("message", m => {
+  if(m.content.startsWith("~")) {
+    m.channel.send("Hello there.");
+  } else {
+    return console.log(m.content);
+  };
 });
 
 bot.login(process.env.FuckYouToo);
